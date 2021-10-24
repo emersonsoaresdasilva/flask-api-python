8c93137c94bd82dd529d037634c17dee079fc79c
emersonsoaresdasilva <emerson.soares@aluno.faculdadeimpacta.com.br>
2021-10-24 04:42:36 -0300

    Manipula dados dos produtos em CSV

--- app/app.py
+++ app/app.py
@@ -1,3 +1,4 @@
+import csv
 from flask import Flask, json, jsonify, request
 
 app = Flask(__name__)
@@ -11,6 +12,30 @@ def index():
 @app.route('/get-all', methods=['GET'])
 def get_all():
     return jsonify(db) if db != [] else jsonify({'message': 'nenhum registro encontrado!'})
+
+@app.route('/import-csv', methods=['GET'])
+def import_csv():
+    try:
+        with open('data.csv', 'r') as csv_file:
+            csv_reader = csv.DictReader(csv_file, delimiter=';')
+            data = list(csv_reader)
+            for produto in data:
+                db.append(produto)
+            return jsonify({'status': 'ok', 'message': 'dados importados com sucesso.'})
+    except Exception:
+        return jsonify({'status': 'error', 'message': 'verifique os dados informados e tente novamente!'})
+
+@app.route('/export-csv', methods=['GET'])
+def export_csv():
+    try:
+        with open('products.csv', 'w', newline='') as csv_file:
+            csv_writer = csv.DictWriter(csv_file, fieldnames=['id', 'name', 'price', 'quantity'], delimiter=';')
+            csv_writer.writeheader()
+            for product in db:
+                csv_writer.writerow(product)
+            return jsonify({'status': 'ok', 'message': 'dados exportados com sucesso.'})
+    except Exception:
+        return jsonify({'status': 'error', 'message': 'verifique os dados informados e tente novamente!'})
 
 if __name__ == '__main__':
     app.run(debug=True)
